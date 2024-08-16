572c57d98b738cd2fb7514a2e3fe61081b975407
Alexandra Baquero <alexandra.baquero@csgi.com>
2024-08-15 21:26:57 -0500

    Property Component

---
+++ AppSaleFront/app/property/[id]/page.js
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+async function GetProperty(id){
+  const response = await axios.get('http://127.0.0.1:8004/properties/'+ {id})
+  console.log(response)
+  return response.data
+}
+
+
+export default function Main ({params}){
+  const id = params.id
+  const [property, setProperty] = useState('')
+
+  useEffect(() => {
+    GetProperty({id}).then(propertyItem=>{
+        console.log("Que datos tengo property", propertyItem)
+        setProperties(propertyItem)
+    }).catch (e=>{
+     console.log('error ',e.message)
+    })
+  },[] )
+
+  console.log("Este es el id" + {id})
+ return <p>Componente</p>
+
+}
