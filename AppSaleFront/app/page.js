680d8165c5f52ce1559237cf54d4897d0430d246
Cristian Torres <cristian.torres19@hotmail.com>
2024-09-03 14:23:14 -0500

    added annimations and improved styles

--- AppSaleFront/app/page.js
+++ AppSaleFront/app/page.js
@@ -1,9 +1,11 @@
 "use client"
-import Image from "next/image";
 import styles from "./page.module.css";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
+import PropertiesList from "@/components/PropertiesList";
+import {formatPricing} from "@/helpers/numbers";
+import Head from "next/head";
 
 async function fetchProperties() {
     const response = await axios.get('http://127.0.0.1:8004/properties')
@@ -29,7 +31,11 @@ export default function Home() {
         fetchProperties().then(propertiesList => {
             //El then espera a que termine el proceso asincrono
             console.log("Que datos tengo properties list", propertiesList)
-            setProperties(propertiesList)
+            setProperties(propertiesList.map(c => ({
+                ...c,
+                minPrice: formatPricing(c.minPrice),
+                maxPrice: formatPricing(c.maxPrice)
+            })))
         }).catch(e => {
             console.log('error ', e.message)
         })
@@ -37,28 +43,15 @@ export default function Home() {
 
 
     return (
-        <main className={styles.main}>
-            <h1>HouseLy App</h1>
-            <h4>Contador == {count}</h4>
-            <button onClick={() => setCount(c => c + 1)}>Aumente Contador</button>
-            <section className="properties">
-                <div className={styles.properties}>{properties && properties.map(property => (
-                    <div onClick={() => handleCardClick(property._id)} className={styles.card}
-                         style={{backgroundImage: `url(${property.image})`}}>
-                        <div className={styles.information}>
-                            <h5>{property.name}</h5>
-                            <p>{property.description}</p>
-                            <div className="price">
-                                <p>max price : <b>{property.maxPrice}</b></p>
-                                <p>min price : <b>{property.minPrice}</b></p>
-                            </div>
-                            <p>{property.offer ? '😊' : '🤦‍♂️'}</p>
-                            <p>city {property.city}</p>
-                            <p>address {property.address}</p>
-                        </div>
-                    </div>
-                ))}</div>
-            </section>
-        </main>
+        <>
+            <Head>
+                <title>HouseLy App - Home</title>
+                <meta name="description" content="Browse houses and properties available for sale or rent."/>
+            </Head>
+            <main className={styles.main}>
+                <h1>HouseLy App</h1>
+                {properties && <PropertiesList properties={properties}/>}
+            </main>
+        </>
     );
 }
