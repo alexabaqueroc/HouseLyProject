b07a8edba80dd2a388611aacc4d51911d6bc10e2
Alexandra Baquero <alexandra.baquero@csgi.com>
2024-11-09 20:26:24 -0500

    alexa changes for splitting rent and sale entities

--- AppSaleBack/db/entities/propertyentity.py
+++ AppSaleBack/db/entities/propertyentity.py
@@ -1,21 +1,69 @@
-from typing import Optional
-
+from typing import List, Literal, Optional
+from enum import Enum
 from bson import ObjectId
 from pydantic import BaseModel, Field, model_validator
 
 
-class PropertyEntity(BaseModel):
-    id: Optional[str] = Field(None, alias='_id')  # Optional for creation
-    name: str
-    image: str
-    description: str
-    maxPrice: float
-    minPrice: float
-    offer: bool
-    city: str
-    area: float
-    address: str
 
+Features = ['Balcony','Garage','Internet']
+
+
+#Define the Enums
+class TypeResidential (str,Enum):
+    house = "house"
+    apto="apto"
+    farm="farm"
+    countryHouse = "countryHouse"
+
+class TypePurpose (str,Enum):
+    rent = "rent"
+    sale="sale"
+
+class Season(BaseModel):
+    name: Literal['alta', 'media', 'baja']
+    price: int
+    selected: bool
+
+class Prices(BaseModel):
+    priceMin: int
+    priceMax: int
+    selected: bool
+
+class PriceRent(BaseModel):
+    type: str
+    selected: bool
+    season: Optional[List[Season]] = None  # List of Temporada or None
+
+class PriceSale(BaseModel):
+    type: str
+    selected: bool
+    priceList: Optional[List[Prices]] = None  # List of prices
+    
+class PropertySaleEntity(BaseModel):
+    id: Optional[str] = Field(None, alias='_id')  # Optional for creation
+    typeResidencial: TypeResidential
+    image: str
+    video: str
+    description: str
+    room:int
+    bedroom:int
+    bath:int
+    sqft: float
+    priceSale:List[PriceSale]
+  
+class PropertyRentEntity(BaseModel):
+    id: Optional[str] = Field(None, alias='_id')  # Optional for creation
+    typeResidencial: TypeResidential
+    personNo: Optional[int]
+    image: str
+    video: str
+    description: str
+    room:int
+    bedroom:int
+    bath:int
+    sqft: float
+    priceRent:List[PriceRent]
+  
     @model_validator(mode='before')
     def convert_objectid(cls, values):
         if '_id' in values and isinstance(values['_id'], ObjectId):
--- appSale/db/db.py
+++ appSale/db/db.py
@@ -2,6 +2,5 @@ import motor.motor_asyncio
 
 from ..config.config import settings
 
-print(f'{settings.MONGO_URI}😁😠')
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_URI)
 database = client[settings.MONGO_DB]
